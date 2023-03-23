6a48578fab44d4e8296e6c3fe5f775f229a5b1fe
emrekayik <emrekayik1905@gmail.com>
2023-03-24 00:14:05 +0300

    darkMode geliştirildi

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,6 @@ module.exports = {
   darkMode: 'class',
   theme: {
     extend: {
-      colors: {
-        primary: '#2c5282',
-        secondary: '#718096',
-        darkPrimary: '#cbd5e0',
-        darkSecondary: '#a0aec0',
-      },
     },
   },
   plugins: [],
