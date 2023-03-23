62de08f20e2bd0d97af8c29beb86d6ae8beca665
emrekayik <emrekayik1905@gmail.com>
2023-03-23 21:23:08 +0300

    Google Analytics added

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,11 +8,15 @@ export default function Document() {
       <Head>
         <Script async src="https://www.googletagmanager.com/gtag/js?id=G-2NBEJLXNHX"></Script>
         <Script>
+          {`
           window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments)}
+          function gtag(){
+            dataLayer.push(arguments)
+          }
           gtag('js', new Date());
 
           gtag('config', 'G-2NBEJLXNHX');
+          `}
         </Script>
       </Head>
       <body>
