857fc043ddc620cd9e77043a14dc3b38192b4fb5
Emre Kayık <54414075+emrekayik@users.noreply.github.com>
2023-07-17 16:39:23 +0000

    analytics ayarları

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -22,15 +22,7 @@ export default function App({ Component, pageProps }) {
               async
               src="https://www.googletagmanager.com/gtag/js?id=G-2NBEJLXNHX"
             />
-            <Script id="google-analytics">
-              {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments)}
-            gtag('js', new Date());
-
-            gtag('config', 'G-2NBEJLXNHX');
-            `}
-            </Script>
+            
             <Header />
             <motion.div
               key={router.route}
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,16 +7,14 @@ export default function Document() {
     <Html lang="tr-TR">
       <Head>
         <meta name="google-site-verification" content="gsAzII9ttXYe5b-hlxxpoD96wedsePNDTiMI0Y_WVGU" />
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-2NBEJLXNHX"></Script>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-5BL3WZJY8G"></Script>
         <Script>
           {`
           window.dataLayer = window.dataLayer || [];
-          function gtag(){
-            dataLayer.push(arguments)
-          }
+          function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-
-          gtag('config', 'G-2NBEJLXNHX');
+        
+          gtag('config', 'G-5BL3WZJY8G');
           `}
         </Script>
       </Head>
